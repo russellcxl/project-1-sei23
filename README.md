ec3f6e3024f8728dccd45f8d550ba79c3d5c1886
Russell Chan <russellcxl@gmail.com>
2020-06-26 17:38:34 +0800

    fixed checker bug, added sweetalerts

--- README.md
+++ README.md
@@ -1,29 +1,45 @@
 # SUDOKU
 
-SEI-23: Project 1
+## SEI-23: Project 1
 
 
 
-Basic:
-    1. Create board (UI and array for checking)
-    2. Attach listeners
-    3. Create button for starting a new game / resetting board
-    4. Populate UI board using values from the backend array
+### Basic:
+1. Create board (UI and array for checking)
+2. Attach listeners
+3. Create button for starting a new game / resetting board
+4. Populate UI board using values from the backend array
 
 
-Further:
-    1. Create checker to see if all input numbers are correct i.e. does not repeat in the row, the column and the 9x9 box the number is in.
+### Further:
+1. Create checker to see if all input numbers are correct i.e. does not repeat in the row, the column and the 9x9 box the number is in.
 
-    2. Create solver
+2. Create solver
 
-        => Loop through array to find empty boxes
-        => Loop through 1-9 and use checker to admit the first valid number
-        => After admitting valid number, call the function again so that it moves to the next empty box
-        => Continues until no empty boxes are left
-        => If, however, the function reaches a box that cannot be filled with any number from 1-9, box will remain empty and the function will return a false; in which case it goes back to the previous box and increases that number by one before proceeding thence
+    - Loop through array to find empty boxes
+    - Loop through 1-9 and use checker to admit the first valid number
+    - After admitting valid number, call the function again so that it moves to the next empty box
+    - Continues until no empty boxes are left
+    - If, however, the function reaches a box that cannot be filled with any number from 1-9, box will remain empty and the function will return a false; in which case it goes back to the previous box and increases that number by one before proceeding thence
+
+    - Visualisation of the function:
+
+    ![https://en.wikipedia.org/wiki/Backtracking#/media/File:Sudoku_solved_by_bactracking.gif](https://en.wikipedia.org/wiki/Backtracking#/media/File:Sudoku_solved_by_bactracking.gif)
 
 
-Further: 
-    1. Add various animations -- numbers falling into board, shaking the wrong numbers when check button is clicked, etc
-    2. Add background music??
-    3. Make pretty the entire set
+### Further: 
+1. Add various animations -- numbers falling into board, shaking the wrong numbers when check button is clicked, etc
+2. Add background music??
+3. Make pretty the entire set
+4. Give options to draw a '?' or multiple numbers in a box. Also allow user to grey out boxes if they are setting their own puzzle.
+
+
+### References: 
+- For Sweet Alert
+    - https://web.dev/promises/
+    - https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-promise-27fc71e77261
+    - https://medium.com/javascript-in-plain-english/async-await-javascript-5038668ec6eb
+- For Sudoku checking & solving
+    - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+    - https://www.geeksforgeeks.org/sudoku-backtracking-7/
+    - https://stackoverflow.com/questions/42736648/sudoku-solver-in-js
--- script.js
+++ script.js
@@ -5,14 +5,15 @@
 
 
 let boardState = 0; //0 = no errors, 1 = there are errors
-let gridArr = [];
+let boardArr = [];
 
 for (let i = 0 ; i < 9 ; i++) {
-    gridArr.push([]);
+    boardArr.push([]);
     for (let j = 0 ; j < 9 ; j++) {
-        gridArr[i].push('');
+        boardArr[i].push('');
     }
 }
+
 
 
 //---------------------------------------- CREATING UI BOARD ----------------------------------------
@@ -60,11 +61,11 @@ for (let i = 5 ; i < 78 ; i+=9) {
 }
 
 
-//updating gridArr when board is updated
+//updating boardArr when board is updated
 
 for (let i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener('change', function() {
-        gridArr[Math.floor(i/9)][i%9] = boxes[i].value;
+        boardArr[Math.floor(i/9)][i%9] = boxes[i].value;
     });
 }
 
@@ -75,17 +76,86 @@ for (let i = 0; i < boxes.length; i++) {
 
 
 
+class Board {
+    constructor(difficulty) {
+        this.difficulty = '';
+    }
+
+    populate() { //not sure if i'm using classes right; all the boards can be put into an object for easy retrieval
+
+        if (this.difficulty === 'easy') {
+            boardArr = [
+                ['', '', '', '2', '', '1', '8', '4', ''],
+                ['', '8', '', '', '3', '', '', '', ''],
+                ['2', '', '9', '', '', '6', '', '3', '5'],
+                ['', '', '3', '', '5', '4', '', '9', ''],
+                ['', '', '8', '1', '', '3', '4', '', ''],
+                ['', '7', '', '8', '6', '', '1', '', ''],
+                ['8', '4', '', '9', '', '', '3', '', '2'],
+                ['', '', '', '', '7', '', '', '8', ''],
+                ['', '6', '2', '3', '', '8', '', '', '']
+            ];
+        }
+
+        else if (this.difficulty === 'medium') {
+            boardArr = [
+                ['', '2', '', '6', '', '8', '', '', ''],
+                ['5', '8', '', '', '', '9', '7', '', ''],
+                ['', '', '', '', '4', '', '', '', ''],
+                ['3', '7', '', '', '', '', '5', '', ''],
+                ['6', '', '', '', '', '', '', '', '4'],
+                ['', '', '8', '', '', '', '', '1', '3'],
+                ['', '', '', '', '2', '', '', '', ''],
+                ['', '', '9', '8', '', '', '', '3', '6'],
+                ['', '', '', '3', '', '6', '', '9', '']
+            ];
+        }
+
+        else if (this.difficulty === 'hard') {
+            boardArr = [
+                ['', '1', '', '', '8', '', '', '', ''],
+                ['', '', '2', '4', '', '', '9', '', ''],
+                ['', '', '8', '6', '', '', '', '', '7'],
+                ['2', '7', '', '', '', '', '', '1', ''],
+                ['', '4', '1', '', '5', '', '7', '2', ''],
+                ['', '8', '', '', '', '', '', '6', '9'],
+                ['4', '', '', '', '', '5', '2', '', ''],
+                ['', '', '6', '', '', '9', '3', '', ''],
+                ['', '', '', '', '4', '', '', '9', '']
+            ];
+        }
+        else {
+            boardArr = [
+                ['', '', '', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', '', '', ''],
+                ['', '', '', '', '', '', '', '', '']
+            ];
+        }
+        
+        populateBoard();
+        greyStartingBoxes();
+    }  
+}
+
+
+
 function easyBoard() {
-    gridArr = [
-        ['', '2', '', '6', '', '8', '', '', ''],
-        ['5', '8', '', '', '', '9', '7', '', ''],
-        ['', '', '', '', '4', '', '', '', ''],
-        ['3', '7', '', '', '', '', '5', '', ''],
-        ['6', '', '', '', '', '', '', '', '4'],
-        ['', '', '8', '', '', '', '', '1', '3'],
-        ['', '', '', '', '2', '', '', '', ''],
-        ['', '', '9', '8', '', '', '', '3', '6'],
-        ['', '', '', '3', '', '6', '', '9', '']
+    boardArr = [
+        ['', '', '', '2', '', '1', '8', '4', ''],
+        ['', '8', '', '', '3', '', '', '', ''],
+        ['2', '', '9', '', '', '6', '', '3', '5'],
+        ['', '', '3', '', '5', '4', '', '9', ''],
+        ['', '', '8', '1', '', '3', '4', '', ''],
+        ['', '7', '', '8', '6', '', '1', '', ''],
+        ['8', '4', '', '9', '', '', '3', '', '2'],
+        ['', '', '', '', '7', '', '', '8', ''],
+        ['', '6', '2', '3', '', '8', '', '', '']
     ];
     populateBoard();
     greyStartingBoxes();
@@ -93,17 +163,19 @@ function easyBoard() {
 easyBoard();
 
 
+
 //populate HTML board according to board array ie the backend board
 function populateBoard() {
-    for (let i = 0; i < gridArr.flat().length; i++) {
-        boxes[i].value = gridArr.flat()[i];
+    for (let i = 0; i < boardArr.flat().length; i++) {
+        boxes[i].value = boardArr.flat()[i];
     }
 }
 
 
 function greyStartingBoxes() {
-    for (let i = 0; i < gridArr.flat().length; i++) {
-        if (gridArr.flat()[i] !== '') {
+    for (let i = 0; i < boardArr.flat().length; i++) {
+        boxes[i].disabled = false;
+        if (boardArr.flat()[i] !== '') {
             // boxes[i].style.backgroundColor = 'grey';
             boxes[i].disabled = true;
             boxes[i].style.fontWeight = 700;
@@ -117,26 +189,111 @@ function greyStartingBoxes() {
 
 
 
-let checkButton = document.getElementById('check');
-let solveButton = document.getElementById('solve');
-let resetButton = document.getElementById('reset');
+$("document").ready(function() { //onload, allow user to choose difficulty
+    
+    // const inputOptions = new Promise((resolve) => {
+    //     setTimeout(() => {
+    //         resolve({
+    //             '#ff0000': 'Red',
+    //             '#00ff00': 'Green',
+    //             '#0000ff': 'Blue'
+    //         })
+    //     }, 1000)
+    // })
+      
+    // const { value: color } = await Swal.fire({
+    //     title: 'Select color',
+    //     input: 'radio',
+    //     inputOptions: inputOptions,
+    //     inputValidator: (value) => {
+    //         if (!value) {
+    //             return 'You need to choose something!'
+    //         }
+    //     }
+    // })
+      
+    // if (color) {
+    //     Swal.fire({ html: `You selected: ${color}` })
+    // }
 
-checkButton.addEventListener('click', function() {
+});
+
+
+
+let newButton = document.getElementById('difficulty'),
+    checkButton = document.getElementById('check'),
+    solveButton = document.getElementById('solve'),
+    resetButton = document.getElementById('reset');
+
+
+newButton.addEventListener('click', function() {
+    let board = new Board('empty');
+    board.populate();
+})
+
+
+checkButton.addEventListener('click', function() {   
     boardState = 0;
     checkRows();
     checkCols();
     checkLargeBox();
-    boardState == 1 ? alert('Something is wrong') : alert('All good')
+
+    if (boardState == 1) {
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: `You've got a mistake somewhere`,
+            showConfirmButton: false,
+            timer: 1200
+        })
+    }
+    else {
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'All good!',
+            showConfirmButton: false,
+            timer: 900
+        })
+    }
 });
 
-solveButton.addEventListener('click', function() {
-    solve();
-    populateBoard();
-})
+
+solveButton.addEventListener('click', function() {    
+    Swal.fire({
+        title: 'Giving up already?',
+        text: "This will show you the completed board",
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'End it'
+        })
+        .then((result) => {
+            if (result.value) {
+                solve();
+                populateBoard();
+            }
+        });   
+});
+
 
 resetButton.addEventListener('click', function() {
-    easyBoard();
-})
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "This will reset the board to its original state",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, reset it!'
+        })
+        .then((result) => {
+            if (result.value) {
+            easyBoard();
+            }
+        });
+});
 
 
 
@@ -145,11 +302,11 @@ resetButton.addEventListener('click', function() {
 
 
 function checkRows() {
-    for (let i = 0; i < gridArr.length; i++) {
-        for (let j = 0 ; j < gridArr[i].length ; j++)  {
-            for (let k = j + 1; k < gridArr[i].length; k++) {
-                if (gridArr[i][j] === gridArr[i][k] && gridArr[i][j] !== '') boardState = 1;               
-            }
+    for (let i = 0; i < 9; i++) {
+        let arr = boardArr[i].filter(x => x.toString().match(/\d/)); 
+        if (arr.length != new Set(arr).size) {
+            boardState = 1;
+            //console.log('wrong');
         }
     }
 }
@@ -157,20 +314,20 @@ function checkRows() {
 
 
 function checkCols() {
-    let arrOfColumns = [];
-    for (let i = 0; i < gridArr.length; i++) {
+    let columns = [];
+    for (let i = 0; i < 9; i++) {
         let tempArr = [];
-        for (let j = 0 ; j < gridArr.length ; j++) {
-            tempArr.push(gridArr[j][i]);
+        for (let j = 0 ; j < 9; j++) {
+            tempArr.push(boardArr[j][i]);
         }
-        arrOfColumns.push(tempArr);    
+        columns.push(tempArr);    
     }
 
-    for (let i = 0; i < arrOfColumns.length; i++) {
-        for (let j = 0 ; j < arrOfColumns[i].length ; j++)  {
-            for (let k = j + 1; k < arrOfColumns[i].length; k++) {
-                if (arrOfColumns[i][j] === arrOfColumns[i][k] && arrOfColumns[i][j] !== '') boardState = 1;               
-            }
+    for (let i = 0; i < 9; i++) {
+        let arr = columns[i].filter(x => x.toString().match(/\d/));
+        if (arr.length != new Set(arr).size) {
+            boardState = 1;
+            //console.log('wrong');
         }
     }
 }
@@ -178,60 +335,56 @@ function checkCols() {
 
 
 function checkLargeBox() {
-    let arrOfLargeBoxes = [];
-    for (let i = 0; i < gridArr.length; i+=3) {
-
+    let largeBoxes = [];
+    for (let i = 0; i < 9; i++) {
         let tempArr = [];
-        for (let j = i; j < i+3; j++) {
-            for (let k = 0; k < 3; k++) {
-                tempArr.push(gridArr[j][k]);
-            }
+        for (let j = 0 ; j < 9; j++) {
+            let r = Math.floor(i / 3) * 3 + Math.floor(j / 3); // 012012012, 345345345, 678678678
+            let c = (i % 3 * 3) + (j % 3);
+            tempArr.push(boardArr[r][c]);
         }
-        arrOfLargeBoxes.push(tempArr);
-
-        tempArr = [];
-        for (let j = i; j < i+3; j++) {
-            for (let k = 3; k < 6; k++) {
-                tempArr.push(gridArr[j][k]);
-            }
-        }
-        arrOfLargeBoxes.push(tempArr);
-
-        tempArr = [];
-        for (let j = i; j < i+3; j++) {
-            for (let k = 6; k < 9; k++) {
-                tempArr.push(gridArr[j][k]);
-            }
-        }
-        arrOfLargeBoxes.push(tempArr);
+        largeBoxes.push(tempArr);
     }
 
-    for (let i = 0; i < arrOfLargeBoxes.length; i++) {
-        for (let j = 0; j < arrOfLargeBoxes[i].length ; j++)  {
-            for (let k = j + 1; k < arrOfLargeBoxes[i].length; k++) {
-                if (arrOfLargeBoxes[i][j] === arrOfLargeBoxes[i][k] && arrOfLargeBoxes[i][j] !== '') boardState = 1;               
-            }
+    for (let i = 0; i < 9; i++) {
+        let arr = largeBoxes[i].filter(x => x.toString().match(/\d/));
+        if (arr.length != new Set(arr).size) {
+            boardState = 1;
+            //console.log('wrong');
         }
     }
 }
 
 
 
-
 //---------------------------------------- SOLVER ----------------------------------------
 
 
-//for [0][0] --> check [0][X] , [X][0] , 
 
 // [0-2][0-2]  [0-2][3-5]  [0-2][6-8]
 // [3-5][0-2]  [3-5][3-5]  [3-5][6-8]
 // [6-8][0-2]  [6-8][3-5]  [6-8][6-8]
 
 
+// function isCorrect(row, col, val) { //using this method causes massive lag; due to stacking of functions maybe?
+//     boardArr[row][col] = `${val}`;
+//     checkRows();
+//     checkCols();
+//     checkLargeBox();
+//     boardArr[row][col] = '';
+//     if (boardState == 1) {
+//         boardState = 0;
+//         return false;
+//     }
+//     return true;
+// }
+
+
+
 function isCorrect(row, col, val) {
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
-            if (gridArr[row][j] == val || gridArr[i][col] == val) {
+            if (boardArr[row][j] == val || boardArr[i][col] == val) {
                 return false;
             }
         }
@@ -242,7 +395,7 @@ function isCorrect(row, col, val) {
 
     for (let i = r; i < r + 3; i++) {
         for (let j = c; j < c + 3; j++) {
-            if (gridArr[i][j] == val) {
+            if (boardArr[i][j] == val) {
                 return false;
             } 
         }
@@ -251,18 +404,19 @@ function isCorrect(row, col, val) {
 }
 
 
+
 function solve() {
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
-            if (gridArr[i][j] == '') {
+            if (boardArr[i][j] == '') {
                 for (let k = 1; k <= 9; k++) {
                     if (isCorrect(i, j, k)) {
-                        gridArr[i][j] = k;
-                        if (solve()){
+                        boardArr[i][j] = k;
+                        if (solve()) {
                             return true;
                         }
                         else {
-                            gridArr[i][j] = '';
+                            boardArr[i][j] = '';
                         }
                     }
                 }
@@ -275,27 +429,10 @@ function solve() {
 
 
 
-// function isValidSudoku(grid) {
-//     let seenRow = {},
-//         seenCol = {},
-//         seenSubBox = {},
-//         seen = {};
 
-//     for (let row = 0; row < 9; row++) {
-//         for (let col = 0; col < 9; col++) {
-//             let value = grid[row][col];
-//             if (!(value === '.')) {
-//                 let rowKey = `${row}-${value}`,
-//                     colKey = `${col}-${value}`,
-//                     boxKey = `${Math.floor(row/3)}-${value}-${Math.floor(col/3)}`
+// ------------------------------------ ANIMATIONS ------------------------------------ //
 
-//                 if (seenRow[rowKey] || seenCol[colKey] || seenSubBox[boxKey]) {
-//                     return false;
-//                 }               
-//                 seenRow[rowKey] = true;
-//                 seenCol[colKey] = true;
-//                 seenSubBox[boxKey] = true;
-//             }
-//         }
-//     }
-// }
+
+
+
+
